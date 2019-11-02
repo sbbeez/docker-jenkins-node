6afc11ed139e79f921c0657bede381de1ef07d16
SenthilBalaji <senthil.g.balaji@gmail.com>
2019-11-03 00:20:47 +0530

    removed dist

--- dist/controllers/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user_controller_1 = require("./user.controller");
-exports.UserController = user_controller_1.UserController;
--- dist/routes/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user_route_1 = require("./user.route");
-exports.UserRoute = user_route_1.UserRoute;
